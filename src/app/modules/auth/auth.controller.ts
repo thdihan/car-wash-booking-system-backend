b9a13ebadc91f211a7a3a67cc0b2f45bab503f96
thdihan <thdihan@outlook.com>
2024-06-15 10:32:32 +0600

    AuthController: Signup function done

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AuthService } from './auth.service';
+
+const signup = catchAsync(async (req, res) => {
+    const result = await AuthService.createUserIntoDB(req.body);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'User is created successfully',
+        data: result,
+    });
+});
+
+export const AuthController = {
+    signup,
+};
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,22 +1,22 @@
-import { UserService } from './user.service';
-import sendResponse from '../../utils/sendResponse';
-import httpStatus from 'http-status';
-import catchAsync from '../../utils/catchAsync';
+// import { UserService } from './user.service';
+// import sendResponse from '../../utils/sendResponse';
+// import httpStatus from 'http-status';
+// import catchAsync from '../../utils/catchAsync';
 
-const createStudent = catchAsync(async (req, res) => {
-  const { password, student: studentData } = req.body;
-  // const zodParsedData = studentValidationSchema.parse(studentData);
+// const createStudent = catchAsync(async (req, res) => {
+//   const { password, student: studentData } = req.body;
+//   // const zodParsedData = studentValidationSchema.parse(studentData);
 
-  const result = await UserService.createStudentIntoDB(password, studentData);
+//   const result = await UserService.createStudentIntoDB(password, studentData);
 
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: 'Student is created successfully',
-    data: result,
-  });
-});
+//   sendResponse(res, {
+//     statusCode: httpStatus.OK,
+//     success: true,
+//     message: 'Student is created successfully',
+//     data: result,
+//   });
+// });
 
-export const UserController = {
-  createStudent,
-};
+// export const UserController = {
+//   createStudent,
+// };
