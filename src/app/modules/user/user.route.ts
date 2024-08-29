ea096cba558a56589d4bd08f49c4ee0788d9049e
thdihan <thdihan@outlook.com>
2024-08-29 17:54:03 +0600

    Route guard updated

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 
 import { UserController } from './user.controller';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
-router.get('/', UserController.getPersonalData);
+router.get(
+    '/',
+    auth(USER_ROLE.admin, USER_ROLE.user),
+    UserController.getPersonalData,
+);
 
 export const UserRoutes = router;
